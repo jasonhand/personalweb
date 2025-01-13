70040363c45ef3c43605b90ee2a656c515335e2a
jasonhand <jasonhand24@gmail.com>
2025-01-13 13:43:58 -0700

    Try again

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,34 +4,3 @@ import Script from "next/script";
 export function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
-
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body>
-        <Script 
-          src="https://www.datadoghq-browser-agent.com/us1/v5/datadog-rum.js" 
-          strategy="beforeInteractive"
-        />
-        <Script id="datadog-init" strategy="afterInteractive">
-          {`
-            window.DD_RUM && window.DD_RUM.init({
-              clientToken: 'puba67520ccd7c8bd2403f3ff782862386a',
-              applicationId: '845079cb-4226-4831-a71e-8873c022a143',
-              site: 'datadoghq.com',
-              service: 'jasonhand.com',
-              env: 'prod',
-              sessionSampleRate: 100,
-              sessionReplaySampleRate: 100,
-              trackUserInteractions: true,
-              trackResources: true,
-              trackLongTasks: true,
-              defaultPrivacyLevel: 'mask-user-input',
-            });
-          `}
-        </Script>
-        {children}
-      </body>
-    </html>
-  );
-}
