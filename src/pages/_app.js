5d915907cf7372c83878b7058c8c711aa548df8a
Jason Hand <jason.hand@datadoghq.com>
2025-01-23 14:57:39 -0700

    adding user session tracking

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,19 +5,23 @@ import { datadogRum } from '@datadog/browser-rum';
 datadogRum.init({
     applicationId: '845079cb-4226-4831-a71e-8873c022a143',
     clientToken: 'puba67520ccd7c8bd2403f3ff782862386a',
-    // `site` refers to the Datadog site parameter of your organization
-    // see https://docs.datadoghq.com/getting_started/site/
     site: 'datadoghq.com',
     service: 'jasonhand.com',
     env: 'production',
-    // Specify a version number to identify the deployed version of your application in Datadog
-    // version: '1.0.0',
+    version: '1.0.0',
     sessionSampleRate: 100,
     sessionReplaySampleRate: 100,
     trackUserInteractions: true,
     trackResources: true,
     trackLongTasks: true,
     defaultPrivacyLevel: 'mask-user-input'
+});
+
+datadogRum.setUser({
+    id: '1234',
+    name: 'John Doe',
+    email: 'john@doe.com',
+    plan: 'premium'
 });
 
 export default function MyApp({ Component, pageProps }) {
